b5cb2c1d57edb9987d401122128d10f020f32f6c
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-06-03 21:45:05 +0700

    1.3 : Cấu Hình folder views lần 1

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get("/" , (req, res) => {
-    res.render("client/pages/home/index.pug");
+    res.render("client/pages/home/index.pug" , {
+        pageTitle : "Trang Chủ"
+    });
 })
 app.get("/products" , (req, res) => {
-    res.render("client/pages/products/index.pug");
+    res.render("client/pages/products/index.pug" , {
+        pageTitle :"Trang Sản Phẩm"
+    });
 })
 app.listen(port , () => {
     console.log(`Đang chạy cổng 3000`);
