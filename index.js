67e5f690bf46194645f477f738c53045a3a1b796
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-06-05 21:12:28 +0700

    1.7 : Nhúng Boostrap 4 và nhúng file tĩnh

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-console.log(process.env.PORT);
 const routeClient = require("./routes/client/index.route");
 const port = process.env.PORT;
 
@@ -9,7 +8,7 @@ const port = process.env.PORT;
 
 app.set('views', './views');
 app.set('view engine', 'pug');
-
+app.use(express.static("public"));
 
 routeClient(app);
 
