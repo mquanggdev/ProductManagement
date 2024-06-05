2a796ec46b17e6f82f77b89742771cd8da4d81cd
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-06-05 21:00:18 +0700

    1.6 : Cấu hình file .env

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000 ;
+require('dotenv').config();
+console.log(process.env.PORT);
 const routeClient = require("./routes/client/index.route");
+const port = process.env.PORT;
+
+
 
 app.set('views', './views');
 app.set('view engine', 'pug');
+
 
 routeClient(app);
 
