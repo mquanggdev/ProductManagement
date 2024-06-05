e728614ee87d9a134e36a49e17017b0e23231295
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-06-05 21:32:23 +0700

    1.9 : Cấu hình database vào project

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 require('dotenv').config();
 const routeClient = require("./routes/client/index.route");
 const port = process.env.PORT;
-
+const database = require("./config/database");
 
 
 app.set('views', './views');
@@ -11,6 +11,7 @@ app.set('view engine', 'pug');
 app.use(express.static("public"));
 
 routeClient(app);
+database.connect();
 
 app.listen(port , () => {
     console.log(`Đang chạy cổng 3000`);
