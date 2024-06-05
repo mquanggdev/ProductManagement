d6c82e49b6d0cc75efb55642b687d87ae5c77549
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-06-05 21:50:39 +0700

    2: Sửa giao diện products bên client (lấy từ database)

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,7 +1,16 @@
+const Product = require("../../model/product.model")
 //[GET]/products
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        status:"active",
+        deleted:false
+    });
+    for (const item of products) {
+        item.newPrice = (item.price - (item.price * item.discountPercentage / 100) ).toFixed(0);
+    }
     res.render("client/pages/products/index.pug" , {
-        pageTitle :"Trang Sản Phẩm"
+        pageTitle :"Trang Sản Phẩm",
+        products : products
     });
 }
 // [POST]/products/add
