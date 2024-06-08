a5a5cb8783732b3f1bcdad164ed8efd47b7d5cd3
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-06-08 08:58:03 +0700

    2.1 : Tạo trang tổng quan

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const routeClient = require("./routes/client/index.route");
+const routeAdmin = require("./routes/admin/index.route")
 const port = process.env.PORT;
 const database = require("./config/database");
 
@@ -11,6 +12,7 @@ app.set('view engine', 'pug');
 app.use(express.static("public"));
 
 routeClient(app);
+routeAdmin(app);
 database.connect();
 
 app.listen(port , () => {
