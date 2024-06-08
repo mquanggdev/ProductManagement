98fa8b151d3c09111df1a83ffa74c992e9a2fff0
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-06-08 15:30:55 +0700

    2.5 : Tính năng lọc sản phẩm theo trạng thái

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,9 +1,13 @@
 const Product = require("../../model/product.model");
 //GET /admin/products
 module.exports.index = async (req,res) => {
-    const products = await Product.find({
+    const find = {
         deleted : false
-    });
+    };
+    if(req.query.status) {
+        find.status = req.query.status;
+      }
+    const products = await Product.find(find);
     res.render("admin/pages/products/index.pug" , {
         pageTitle : "Trang Admin Sản Phẩm " , 
         products : products
