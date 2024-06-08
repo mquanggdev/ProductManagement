ca24080935e15c46e5988d031b4f5338fe61eeb8
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-06-08 10:44:05 +0700

    2.4 Lấy data in ra giao diện trang sản phẩm của admin

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,6 +1,11 @@
+const Product = require("../../model/product.model");
 //GET /admin/products
-module.exports.index = (req,res) => {
+module.exports.index = async (req,res) => {
+    const products = await Product.find({
+        deleted : false
+    });
     res.render("admin/pages/products/index.pug" , {
-        pageTitle : "Trang Admin Sản Phẩm "
+        pageTitle : "Trang Admin Sản Phẩm " , 
+        products : products
     });
 }
