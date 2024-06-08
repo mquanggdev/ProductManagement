ec9d5c0656192bceacbd5683c3f73d1467ea9d28
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-06-08 09:17:38 +0700

    2.3 : Xây dựng giao diện cứng bên admin products

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,5 +1,6 @@
 const dashboardRouter = require("./dashboard.route");
-
+const productsRouter = require("./products.route");
 module.exports = (app) =>{
     app.use("/admin/dashboard" , dashboardRouter);
+    app.use("/admin/products" , productsRouter)
 } 
