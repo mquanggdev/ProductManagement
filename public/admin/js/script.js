e11f1805ed1cdf3d108f97d5a37c4760494159ee
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-06-11 14:19:51 +0700

    2.8 : logic front-end : tìm kiếm theo nhiều tiêu chí

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -21,3 +21,21 @@ if (listButtonStatus.length > 0 ){
     buttonCurrent.classList.add("active");
 }
 // end button status
+
+//form search : tìm nhiều tiêu chí như là tìm sản phẩm với trạng thái nào đó.
+const formSearch = document.querySelector("[form-search]");
+if (formSearch){
+    const url = new URL(window.location.href);
+    formSearch.addEventListener("submit" , (event) =>{
+        event.preventDefault();
+        const keyword = event.target.elements.keyword.value ;
+        if (keyword){
+            url.searchParams.set("keyword" ,keyword);
+        }
+        else{
+            url.searchParams.delete("keyword");
+        }
+        window.location.href = url.href;
+    })
+}
+//end form search
