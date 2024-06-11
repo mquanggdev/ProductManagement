2ebf8f66ce3cb636019b3f2e74b5ca050f890acf
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-06-11 13:49:44 +0700

    2.7 : logic backend phần tìm kiếm

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -7,9 +7,16 @@ module.exports.index = async (req,res) => {
     if(req.query.status) {
         find.status = req.query.status;
       }
+    var keyword = "" ;
+    if (req.query.keyword){
+        const regex = new RegExp(req.query.keyword, "i"); // không phân biệt hoa thường. thì biến thứ 2 thêm "i"
+        find.title = regex;
+        keyword = req.query.keyword
+    }
     const products = await Product.find(find);
     res.render("admin/pages/products/index.pug" , {
         pageTitle : "Trang Admin Sản Phẩm " , 
-        products : products
+        products : products,
+        keyword : keyword
     });
 }
