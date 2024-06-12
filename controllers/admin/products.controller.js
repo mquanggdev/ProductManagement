e0793a7fdef94d17d295705aa5b9dc82fc6da1d0
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-06-12 15:14:50 +0700

    3.1 : Tính năng phân trang bên backend

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -18,20 +18,44 @@ module.exports.index = async (req,res) => {
     const find = {
         deleted : false
     };
+    //  trạng thái
     if(req.query.status) {
         find.status = req.query.status;
       }
+    // end  trạng thái
+    
+    // tìm kiếm
     var keyword = "" ;
     if (req.query.keyword){
         const regex = new RegExp(req.query.keyword, "i"); // không phân biệt hoa thường. thì biến thứ 2 thêm "i"
         find.title = regex;
         keyword = req.query.keyword
     }
-    const products = await Product.find(find);
+    // end tìm kiếm
+
+
+    // Phân trang
+    const pagination = {
+        currentPage: 1,
+        limitItems : 4, // mỗi trang có 4 phần tử
+    };
+    if (req.query.page){
+        pagination.currentPage = parseInt(req.query.page);
+    }
+    pagination.skip = (pagination.currentPage - 1) * (pagination.limitItems); // bỏ qua bao nhiêu trang
+    const allProductFind = await Product.countDocuments(find);
+    pagination.totalPage = Math.ceil(allProductFind / pagination.limitItems);
+     // end Phân trang
+    const products = await Product
+        .find(find)
+        .limit(pagination.limitItems)
+        .skip(pagination.skip);
+
     res.render("admin/pages/products/index.pug" , {
         pageTitle : "Trang Admin Sản Phẩm " , 
         products : products,
         keyword : keyword,
-        filterStatus: filterStatus
+        filterStatus: filterStatus,
+        pagination: pagination
     });
 }
