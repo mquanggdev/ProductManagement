d36978892e357ba4158e2ceecf1cf0a9c36fbee6
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-06-27 17:25:15 +0700

    4.9 : Tạo tính năng preview ảnh khi upload ảnh sản phẩm

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -207,3 +207,18 @@ if ( show_alert){
     },time);
 }
 // end show popup
+
+// preview ảnh
+const uploadImage = document.querySelector("[upload-image]");
+if(uploadImage) {
+  const uploadImageInput = uploadImage.querySelector("[upload-image-input]");
+  const uploadImagePreview = uploadImage.querySelector("[upload-image-preview]");
+
+  uploadImageInput.addEventListener("change", () => {
+    const file = uploadImageInput.files[0];
+    if(file) {
+      uploadImagePreview.src = URL.createObjectURL(file);// thằng này nó tạo ra đường link ảo
+    }
+  });
+}
+// end preview ảnh
