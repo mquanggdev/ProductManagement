280e6a5f7680b1bf85cb01617f4475dce53ca5a2
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-07-18 16:36:24 +0700

    Bài 24.2 : Hiển thị menu chung bên client

---
+++ middleware/client/categoryProduct.middleware.js
@@ -0,0 +1,13 @@
+const ProductCategory = require("../../model/products-category.model");
+const createTreeHelper = require("../../helper/createTree.helper");
+module.exports.category = async (req,res,next) => {
+    const categoryProducts = await ProductCategory.find({
+        deleted: false,
+        status: "active"
+      });
+    
+      const newCategoryProducts = createTreeHelper(categoryProducts);
+    
+      res.locals.layoutCategoryProducts = newCategoryProducts
+    next();
+}
