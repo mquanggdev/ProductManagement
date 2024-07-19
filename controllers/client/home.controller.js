a9766261da180f6ec5fd996d985abe4e0e4f53ca
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-07-19 20:33:52 +0700

    Bài 24.2 : Hiển thị sản phẩm nổi bật và sản phẩm mới

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,33 @@
+const Product = require("../../model/product.model");
 // [Get] /
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const productsFeatured = Product
+    .find({
+        featured:"1",
+        deleted:false,
+        status:"active"
+    })
+    .sort({position:"desc"})
+    .limit(6)
+    .select("-description")
+    for (const item of productsFeatured) {
+        item.priceNew = ((1 - item.discountPercentage/100) * item.price).toFixed(0);
+      }
+
+        const productsNew = Product
+    .find({
+        deleted:false,
+        status:"active"
+    })
+    .sort({position:"desc"})
+    .limit(6)
+    .select("-description")
+    for (const item of productsNew) {
+        item.priceNew = ((1 - item.discountPercentage/100) * item.price).toFixed(0);
+      } 
     res.render("client/pages/home/index.pug" , {
-        pageTitle : "Trang Chủ"
+        pageTitle : "Trang Chủ",
+        productsFeature:productsFeatured,
+        productsNew:productsNew
     });
 }
