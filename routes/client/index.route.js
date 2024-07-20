2fb78f00db89e75261369e5012a3997872107200
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-07-20 13:56:15 +0700

    Bài 24.7 : Làm phần Trang tìm kiếm sản phẩm bên client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,9 +1,11 @@
 const homeRouter = require("./home.route");
 const productsRouter = require("./products.route");
+const searchRouter = require("./search.route");
 const categoryProductMiddelware = require("../../middleware/client/categoryProduct.middleware");
 
 module.exports = (app) => {
     app.use(categoryProductMiddelware.category);
     app.use("/" , homeRouter);
     app.use("/products" , productsRouter);
+    app.use("/search" , searchRouter);
 } 
