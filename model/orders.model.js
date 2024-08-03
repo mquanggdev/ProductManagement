f2430838b8c9c0963aa257ed34a2f603e323ea67
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-08-03 11:33:53 +0700

    Bài 26.1 : Tạo collection và model cho phần order

---
+++ model/orders.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+    userInfo : {
+        fullname:String,
+        phone :String,
+        address : String
+    },
+    products: [
+        {
+            productId: String,
+            price :Number,
+            quantity: Number,
+            discountPercentage : Number ,
+        }
+    ]
+}, {
+  timestamps: true
+});
+
+const Order = mongoose.model("Order", orderSchema, "orders");
+
+module.exports = Order;
