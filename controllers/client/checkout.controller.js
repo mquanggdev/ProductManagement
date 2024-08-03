0b6ce77ee50888700ec07deff63173e7f5e0cbaf
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-08-03 15:11:27 +0700

    Bài 26.2 : Lưu data của Order bao gồm thông tin user và thông tin sản phẩm vào db

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -29,3 +29,40 @@ module.exports.index = async (req, res) => {
       cartDetail : cart ,
     });
   }
+
+// [Post] /checkout/order
+module.exports.orderPost = async (req , res) => {
+    console.log(req.body);
+    const userInfo = req.body ;
+    const orderData = {
+        userInfo :userInfo,
+        products : []
+    }
+
+    const cartId = req.cookies.cartId ;
+    const cart = await Cart.findOne({
+            _id : cartId
+        }
+    )
+    for (const product of cart.products) {
+        const productInfo = await Product.findOne({
+            _id : product.productId
+        })
+        orderData.products.push({
+            productId: product.id,
+            price :productInfo.price,
+            quantity: product.quantity,
+            discountPercentage : productInfo.discountPercentage
+        })
+    }
+    const newOrder = new Order(orderData);
+    await newOrder.save() ;
+
+
+    await Cart.updateOne({
+        _id :cartId
+    } ,{
+        products : []
+    })
+    res.redirect(`/checkout/success/${newOrder.id}`) ;
+}
